ed903ce7bb6f43e06c2ed9cc310c1359d171110e
duxiwei <duxiwei@aliyun.com>
2018-08-14 00:30:55 +0800

    test

--- App.js
+++ App.js
@@ -21,8 +21,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        
       </View>
     );
   }
